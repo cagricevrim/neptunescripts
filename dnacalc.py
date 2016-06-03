90151498f5e66a4f9d2d34f28bd3c0e1d6d44f56
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-03 11:49:42 +0200

    dNTP percentages has added

--- dnacalc.py
+++ dnacalc.py
@@ -2,7 +2,16 @@
 
 DNAseq = 'ATGAAC'
 print( 'Sequence ' + DNAseq )
-SeqLenght = len( DNAseq )
-print ( SeqLenght )
+SeqLength = len( DNAseq )
+print ( 'Lenght is ' + str ( SeqLength ) )
+NumberA = DNAseq.count( 'A' )
+NumberT = DNAseq.count( 'T' )
+NumberG = DNAseq.count( 'G' )
+NumberC = DNAseq.count( 'C' )
+print('A: ' + str( NumberA / float ( SeqLength ) ) )
+print('T: ' + str( NumberT / float ( SeqLength ) ) )
+print('G: ' + str( NumberG / float ( SeqLength ) ) )
+print('C: ' + str( NumberC / float ( SeqLength ) ) )
 
+ 
 print "Thanks Thomas..."
