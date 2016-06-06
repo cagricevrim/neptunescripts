6f95be3303e02fe22626b4a98bc9f2d0090d4fac
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-06 12:18:45 +0200

    Molecular weight calculation has added

---
+++ proteincalc.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+ProteinSeq = "FDILSKKFTYAGNR"
+
+AminoDict = {'A': 89.09,'R': 174.20,'N': 132.12,'D': 133.10,'C': 121.15,'Q': 146.15,'E': 147.13,'G': 75.07,'H': 155.16,'I': 131.17,'L': 131.17,'K': 146.19,'M': 149.21,'F': 165.19,'P': 115.13,'S': 105.09,'T': 119.12,'W': 204.23,'Y': 181.19,'V': 117.15,'X': 0.0}
+
+for AminoAcid in ProteinSeq:
+	print ( AminoAcid )
+
+for AminoAcid in ProteinSeq:
+	print( 'AminoAcid: {}, weight: {}'.format( AminoAcid, AminoDict[ AminoAcid ] ) )
+
+MolWeight = 0.0
+
+for AminoAcid in ProteinSeq:
+	print( 'AminoAcid: {}, weight: {}'.format( AminoAcid, AminoDict [AminoAcid]))
+	MolWeight = MolWeight + AminoDict [ AminoAcid ]
+
+print ( 'Molecular Weight: {}'.format(MolWeight))
