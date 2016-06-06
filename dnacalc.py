398ec8b3b3a3246cd3d98cacb899006fef49162a
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-06 10:37:19 +0200

    decimal lenght is formatted and melting temp calculation has added

--- dnacalc.py
+++ dnacalc.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-DNAseq = raw_input ( 'Insert a Sequence ' )
+DNAseq = raw_input ( 'Insert a DNA Sequence ' )
 DNAseq = DNAseq.upper()
 print( 'Sequence ' + DNAseq )
 SeqLength = len( DNAseq )
@@ -9,10 +9,22 @@ NumberA = DNAseq.count( 'A' )
 NumberT = DNAseq.count( 'T' )
 NumberG = DNAseq.count( 'G' )
 NumberC = DNAseq.count( 'C' )
-print('A: ' + str( NumberA / float ( SeqLength ) ) )
-print('T: ' + str( NumberT / float ( SeqLength ) ) )
-print('G: ' + str( NumberG / float ( SeqLength ) ) )
-print('C: ' + str( NumberC / float ( SeqLength ) ) )
 
- 
+print('A: {:.2}'.format(NumberA / float (SeqLength)))
+print('T: {:.2}'.format(NumberT / float (SeqLength)))
+print('G: {:.2}'.format(NumberG / float (SeqLength)))
+print('C: {:.2}'.format(NumberC / float (SeqLength)))
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+
+if SeqLength <= 14:
+	MeltTemp = ( 4 * TotalStrong ) + ( 2 * TotalWeak)
+	print ('Melting Temp: {:.2f}'.format(MeltTemp))
+
+else:
+	MeltTemp = 64.9 + 41 * ( TotalStrong - 16.4 ) / SeqLength 
+	print ('Melting Temp: {:.2f}'.format(MeltTemp))
+
+
 print "Courtesy to Thomas..."
