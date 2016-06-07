60972273b02619632fac6956f6dfed9f00b8e24f
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-07 10:06:31 +0200

    calculation is based on loops

--- dnacalc.py
+++ dnacalc.py
@@ -5,18 +5,35 @@ DNAseq = DNAseq.upper()
 print( 'Sequence ' + DNAseq )
 SeqLength = len( DNAseq )
 print ( 'Lenght is ' + str ( SeqLength ) )
-NumberA = DNAseq.count( 'A' )
-NumberT = DNAseq.count( 'T' )
-NumberG = DNAseq.count( 'G' )
-NumberC = DNAseq.count( 'C' )
+#NumberA = DNAseq.count( 'A' )
+#NumberT = DNAseq.count( 'T' )
+#NumberG = DNAseq.count( 'G' )
+#NumberC = DNAseq.count( 'C' )
 
-print('A: {:.2}'.format(NumberA / float (SeqLength)))
-print('T: {:.2}'.format(NumberT / float (SeqLength)))
-print('G: {:.2}'.format(NumberG / float (SeqLength)))
-print('C: {:.2}'.format(NumberC / float (SeqLength)))
+TotalStrong = 0
+TotalWeak = 0
 
-TotalStrong = NumberG + NumberC
-TotalWeak = NumberA + NumberT
+
+Bases = "CGTA"
+
+for Base in Bases:
+	Count = DNAseq.count (Base)
+	Frequency = Count / float (SeqLength)
+	print ( 'Base: {}, {:.2f}'.format( Base, Frequency ) )
+
+	if Base in 'GC':
+		TotalStrong = TotalStrong + Count
+
+	else:
+		TotalWeak = TotalWeak + Count
+
+#print('A: {:.2}'.format(NumberA / float (SeqLength)))
+#print('T: {:.2}'.format(NumberT / float (SeqLength)))
+#print('G: {:.2}'.format(NumberG / float (SeqLength)))
+#print('C: {:.2}'.format(NumberC / float (SeqLength)))
+
+#TotalStrong = NumberG + NumberC
+#TotalWeak = NumberA + NumberT
 
 if SeqLength <= 14:
 	MeltTemp = ( 4 * TotalStrong ) + ( 2 * TotalWeak)
@@ -25,6 +42,6 @@ if SeqLength <= 14:
 else:
 	MeltTemp = 64.9 + 41 * ( TotalStrong - 16.4 ) / SeqLength 
 	print ('Melting Temp: {:.2f}'.format(MeltTemp))
-
-
-print "Courtesy to Thomas..."
+#
+#
+print "Courtesy to Thomas..." 
